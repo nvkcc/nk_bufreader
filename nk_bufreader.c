4832b253397b5be41c80f12711c43c60a1fdedc9
nguyenvukhang <brew4k@gmail.com>
2026-04-21 10:46:05 +0800

    Reorganize

--- nk_bufreader.c
+++ nk_bufreader.c
@@ -4,8 +4,6 @@
 
 #include <string.h>
 
-#define VALID_LEN(r) (r->end - r->buf)
-
 void nk_bufreader_init(nk_bufreader *r) {
     // Initialize all three pointers to be the same.
     r->newl = r->end = r->buf;
@@ -36,50 +34,30 @@ void nk_bufreader_init(nk_bufreader *r) {
 #define debug_print(r)
 #endif
 
-inline int nk_bufreader_bytes_to_read(nk_bufreader *r) {
-    //  0   1   2   3   4   5   6   7   8   9
-    // Consider a buffer of length 10, and the `end` points to index 4 (i.e.: a
-    // VALID_LEN of 4). Then we can only afford to read 5 bytes (the 5 being
-    // 4..=8) because we need to keep buf[9] as the NUL byte.
-    //
-    // That arithmetic, 5 = 10 - 4 - 1, leads us here:
-    return r->len - VALID_LEN(r) - 1;
+// [ 0   1   2   3   4   5   6   7   8   9 ]
+//   ^^^^valid^^^^   ^end                ^invariant NUL byte
+// Consider a buffer of length 10, and the `end` points to index 4 (i.e.: a
+// VALID_LEN of 4). Then we can only afford to read 5 bytes (the 5 being
+// 4..=8) because we need to keep buf[9] as the NUL byte.
+//
+// That arithmetic, 5 = 10 - 4 - 1, leads us here:
+#define VALID_LEN(r) (r->end - r->buf)
+#define BYTES_TO_READ(r) (r->len - VALID_LEN(r) - 1)
+// Note that this function returns 0 if and only if `end` points to
+// index 9 in the above scenario, which is when it is the last address in
+// the allocated buffer.
 
-    // Note that this function returns 0 if and only if `end` points to
-    // index 9 in the above scenario, which is when it is the last address in
-    // the allocated buffer.
-}
-
-/// Appends data to `r->end`. Works as long the position of `r->end` is valid.
-inline int nk_bufreader_append_data(nk_bufreader *r) {
-    int n = read(r->fd, r->end, nk_bufreader_bytes_to_read(r));
-    switch (n) {
-    case 0: // No bytes were read, and end of file is reached.
-        return 0;
-    case -1: // An error occured in `read()`. See the `errno` variable.
-        return NK_BUFREAD_IO_ERROR;
-    default: // Successful read.
-        *(r->end += n) = '\0';
-        return n;
-    }
-}
-
-// =============================================================================
-// The main logic
-// =============================================================================
-
-/*
- * Recall: S := r->buf, A := r->newl, B := r->end
- * The assumption is that we've just got done consuming (externally) the data in
- * buffer[S..A]. So here's what we'll do:
- * (1.) Clear out the old data by doing memmove S <- A.
- * (2.) Look for the next newline character. If one is found, then we NUL that
- *      and return.
- * (3.) Read more data from the file.
- * (4.) Look for the next newline character.
- *      (4a.) If one is found, then we NUL that and return.
- *      (4b.) Else, the buffer is too small. Send error.
- */
+// Let S := r->buf, A := r->newl, B := r->end.
+// The assumption is that we've just got done consuming (externally) the data in
+// buffer[S..A]. So here's what we'll do:
+// (1.) Clear out the old data by doing memmove S <- A.
+// (2.) Look for the next newline character. If one is found, then we NUL that
+//      and return.
+// (3.) Read more data from the file.
+// (4.) Look for the next newline character.
+//      (4a.) If one is found, then we NUL that and return.
+//      (4b.) Else, the buffer is too small. Send error.
+//
 
 //
 
@@ -111,9 +89,8 @@ int nk_bufreader_next(nk_bufreader *r) {
     debug_print(r);
 
     // (3.)
-    nklog_trace("Call read(%d) at [%d]", nk_bufreader_bytes_to_read(r),
-                VALID_LEN(r));
-    switch (n = read(r->fd, r->end, nk_bufreader_bytes_to_read(r))) {
+    nklog_trace("Call read(%d) at [%d]", BYTES_TO_READ(r), VALID_LEN(r));
+    switch (n = read(r->fd, r->end, BYTES_TO_READ(r))) {
     case 0: // No bytes were read, and end of file is reached.
         if (r->end == r->buf) {
             nklog_trace("\x1b[31mReturn\x1b[m {#2}");
@@ -133,9 +110,12 @@ int nk_bufreader_next(nk_bufreader *r) {
     r->newl = (char *)memchr(r->buf, '\n', sizeof(char) * VALID_LEN(r));
     debug_print(r);
     if (r->newl) {
+        // Strictly speaking, this doesn't need to return the insufficient space
+        // error. However, we're trying to be consistent in the agreement that
+        // the buffer length but be exactly 2 bytes longer than the longest line
+        // to be read.
         if (r->newl + 2 == r->buf + r->len) {
-            *r->buf = '\0';
-            r->end = NULL;
+            *r->buf = '\0', r->end = NULL;
             nklog_trace("\x1b[31mReturn\x1b[m {#4}");
             return NK_BUFREAD_INSUFFICIENT_SPACE;
         }
@@ -143,8 +123,7 @@ int nk_bufreader_next(nk_bufreader *r) {
         nklog_trace("\x1b[31mReturn\x1b[m {#5}");
         return NK_BUFREAD_OK;
     } else if (VALID_LEN(r) + 1 >= r->len) {
-        *r->buf = '\0';
-        r->end = NULL;
+        *r->buf = '\0', r->end = NULL;
         nklog_trace("\x1b[31mReturn\x1b[m {#6}");
         return NK_BUFREAD_INSUFFICIENT_SPACE;
     }
@@ -153,3 +132,4 @@ int nk_bufreader_next(nk_bufreader *r) {
 }
 
 #undef VALID_LEN
+#undef BYTES_TO_READ
